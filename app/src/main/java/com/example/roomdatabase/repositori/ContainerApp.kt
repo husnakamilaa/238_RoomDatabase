4ce72f107e728eb5ae28d95ac5ad05353d61dc80
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2025-11-27 14:25:37 +0700

    feat: deklarasi repositoriSiswa

---
+++ app/src/main/java/com/example/roomdatabase/repositori/ContainerApp.kt
@@ -0,0 +1,10 @@
+package com.example.roomdatabase.repositori
+
+import android.app.Application
+import android.content.Context
+import com.example.roomdatabase.room.DatabaseSiswa
+
+interface ContainerApp {
+    val repositoriSiswa : RepositoriSiswa
+}
+
