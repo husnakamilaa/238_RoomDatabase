c29f76ae94b00f78198c2d6d0b0361a595dd2cd1
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2025-11-27 17:51:53 +0700

    feat: mmemanggil function SiswaApp pada body scaffold yang telah dideklarasikan di peta navigasi

--- app/src/main/java/com/example/roomdatabase/MainActivity.kt
+++ app/src/main/java/com/example/roomdatabase/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.roomdatabase.ui.theme.RoomDatabaseTheme
+import com.example.roomdatabase.view.uicontroller.SiswaApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,8 +21,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             RoomDatabaseTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    SiswaApp(
+                        //name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
