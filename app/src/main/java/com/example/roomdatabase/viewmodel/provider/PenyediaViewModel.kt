30092f1e43a560d02c37213a08a5b0965b649afb
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2025-11-27 17:51:08 +0700

    feat: menambahkan penyedia model dengan factory untuk menyediakan view model

---
+++ app/src/main/java/com/example/roomdatabase/viewmodel/provider/PenyediaViewModel.kt
@@ -0,0 +1,23 @@
+package com.example.roomdatabase.viewmodel.provider
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.roomdatabase.repositori.AplikasiSiswa
+import com.example.roomdatabase.viewmodel.EntryViewModel
+import com.example.roomdatabase.viewmodel.HomeViewModel
+
+object PenyediaViewModel {
+    val factory = viewModelFactory {
+        initializer {
+            HomeViewModel(repositoriSiswa = aplikasiSiswa().container.repositoriSiswa)
+        }
+        initializer {
+            EntryViewModel(repositoriSiswa = aplikasiSiswa().container.repositoriSiswa)
+        }
+    }
+}
+
+fun CreationExtras.aplikasiSiswa(): AplikasiSiswa =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiSiswa)
