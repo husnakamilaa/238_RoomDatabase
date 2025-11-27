da299c1b9b186d55579df0df89520710fef1b056
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2025-11-27 17:47:44 +0700

    feat: menambahkan view model untuk halaman tambah data

---
+++ app/src/main/java/com/example/roomdatabase/viewmodel/EntryViewModel.kt
@@ -0,0 +1,60 @@
+package com.example.roomdatabase.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.roomdatabase.repositori.RepositoriSiswa
+import com.example.roomdatabase.room.Siswa
+
+class EntryViewModel (private val repositoriSiswa: RepositoriSiswa): ViewModel() {
+    var uiStateSiswa by mutableStateOf(value = UIStateSiswa())
+        private set
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa): Boolean {
+        return with(receiver = uiState) {
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
+
+    fun updateUiState(detailSiswa: DetailSiswa) {
+        uiStateSiswa =
+            UIStateSiswa(detailSiswa = detailSiswa, isEntryValid = validasiInput(uiState = detailSiswa))
+    }
+
+    suspend fun saveSiswa() {
+        if(validasiInput()){
+            repositoriSiswa.insertSiswa(siswa = uiStateSiswa.detailSiswa.toSiswa())
+        }
+    }
+}
+
+data class UIStateSiswa(
+    val detailSiswa: DetailSiswa = DetailSiswa(),
+    val isEntryValid: Boolean = false
+)
+
+data class DetailSiswa(
+    val id: Int = 0,
+    val nama: String = "",
+    val alamat: String = "",
+    val telpon: String = "",
+)
+
+fun DetailSiswa.toSiswa(): Siswa = Siswa (
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telpon = telpon
+)
+
+fun Siswa.toUiStateSiswa(isEntryValid: Boolean = false): UIStateSiswa = UIStateSiswa(
+    detailSiswa = this.toDetailSiswa(),
+    isEntryValid = isEntryValid
+)
+
+fun Siswa.toDetailSiswa(): DetailSiswa = DetailSiswa(
+    id = id,
+    nama= nama,
+    alamat = alamat,
+    telpon= telpon
+)
