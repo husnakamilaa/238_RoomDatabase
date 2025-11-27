47f47495efd9baf6f647d63be74e3279f291a622
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2025-11-27 13:10:54 +0700

    feat: membuat database dan deklarasi tabel menggunakan Entity siswa

---
+++ app/src/main/java/com/example/roomdatabase/room/DatabaseSiswa.kt
@@ -0,0 +1,11 @@
+package com.example.roomdatabase.room
+
+import android.content.Context
+import android.provider.CalendarContract
+import androidx.compose.runtime.IntState
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Siswa::class], version = 1, exportSchema = false)
+
