7b6608561a05019e1149a26b52ecd6fff176240f
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2025-12-04 13:22:44 +0700

    feat: mendaftarkan DetailViewModel dulu supaya bisa dibuat dan dikenali

--- app/src/main/java/com/example/roomdatabase/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/roomdatabase/viewmodel/provider/PenyediaViewModel.kt
@@ -1,10 +1,12 @@
 package com.example.roomdatabase.viewmodel.provider
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.roomdatabase.repositori.AplikasiSiswa
+import com.example.roomdatabase.viewmodel.DetailViewModel
 import com.example.roomdatabase.viewmodel.EntryViewModel
 import com.example.roomdatabase.viewmodel.HomeViewModel
 
@@ -15,6 +17,12 @@ object PenyediaViewModel {
         }
         initializer {
             EntryViewModel(repositoriSiswa = aplikasiSiswa().container.repositoriSiswa)
+        }
+        initializer {
+            DetailViewModel(
+                savedStateHandle = this.createSavedStateHandle(),
+                repositoriSiswa = aplikasiSiswa().container.repositoriSiswa
+            )
         }
     }
 }
