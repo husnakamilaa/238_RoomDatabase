ac1ba465c53236ac446f3e44d5e566311c49097b
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2025-12-04 14:21:43 +0700

    feat: menginisialisasi edit view model di penyedia view model supaya terbaca

--- app/src/main/java/com/example/roomdatabase/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/roomdatabase/viewmodel/provider/PenyediaViewModel.kt
@@ -7,6 +7,7 @@ import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.roomdatabase.repositori.AplikasiSiswa
 import com.example.roomdatabase.viewmodel.DetailViewModel
+import com.example.roomdatabase.viewmodel.EditViewModel
 import com.example.roomdatabase.viewmodel.EntryViewModel
 import com.example.roomdatabase.viewmodel.HomeViewModel
 
@@ -22,6 +23,12 @@ object PenyediaViewModel {
             DetailViewModel(
                 savedStateHandle = this.createSavedStateHandle(),
                 repositoriSiswa = aplikasiSiswa().container.repositoriSiswa
+            )
+        }
+        initializer {
+            EditViewModel(
+                this.createSavedStateHandle(),
+                aplikasiSiswa().container.repositoriSiswa
             )
         }
     }
